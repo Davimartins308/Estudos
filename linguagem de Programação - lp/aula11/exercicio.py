7554956eadb143c2a3cae5338ae86dfdcd42370a
Davimartins308 <davimartinscruzeiro2008@gmail.com>
2025-10-21 20:01:32 -0300

    21/10/25

---
+++ linguagem de Programação - lp/aula11/exercicio.py
@@ -0,0 +1,17 @@
+import pyautogui
+import time
+#print(pyautogui.KEYBOARD_KEYS)
+pyautogui.sleep(1)
+pyautogui.press("win")
+pyautogui.write("bloco de notas")
+pyautogui.press("enter")
+pyautogui.sleep(1)
+pyautogui.write("Davi")
+pyautogui.sleep(1)
+
+pyautogui.hotkey("ctrl","w")
+pyautogui.press("right")
+pyautogui.press("enter")
+
+
+
